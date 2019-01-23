9259aa79264b12cadc80f1ec4dcf44abd74b8b71
anuradha <anuradha@SID-PC>
2019-01-22 21:27:47 -0500

    Version 3

--- InClass_Task_Grades/Program.cs
+++ InClass_Task_Grades/Program.cs
@@ -12,8 +12,9 @@ namespace InClass_Age_Program
                 Console.WriteLine("Enter your marks in DIS Class between 0 and 100 ");
                 string input = Console.ReadLine(); // reading from console
                 int marks = int.Parse(input); // Parsing user input
-
-                Cal_grade(marks); // calling the Cal_grade function
+               
+                string Letter = Cal_grade(marks); // calling the Cal_grade function
+                Console.WriteLine("The Letter Grade is: " + String.Format("{0,10}", Letter));
 
                 // exit statement
                 Console.WriteLine("Press any key to exit");
@@ -32,75 +33,85 @@ namespace InClass_Age_Program
         } // end of main
 
         // Defining Cal_grade method
-        private static void Cal_grade(int marks)
+        private static string Cal_grade(int marks)
         {
             // calculate the grade based on the conditions in the syllabus
             // A series of if, else if and else statements to perform the task
+            string caculated_letter = " "; 
             if (marks >= 95 && marks <= 100)
             {
-                Console.WriteLine("The Letter grade is A+" ); // Displaying the results to the console
+                caculated_letter = "A+";
+                //return caculated_letter;
 
             } // end of if (grade >= 95 && grade < 100)
 
             else if (marks >= 90 && marks < 95)
             {
-                Console.WriteLine("The Letter grade is A"); // Displaying the results to the console
+                caculated_letter = "A";
+                //return caculated_letter;
             }
 
             else if (marks >= 87 && marks < 90)
             {
-                Console.WriteLine("The Letter grade is A-"); // Displaying the results to the console
+                 caculated_letter = "A-";
+                //return caculated_letter;
             }
 
             else if (marks >= 84 && marks < 87)
             {
-                Console.WriteLine("The Letter grade is B+"); // Displaying the results to the console
+                 caculated_letter = "B+";
+                //return caculated_letter;
             }
 
             else if (marks >= 80 && marks < 84)
             {
-                Console.WriteLine("The Letter grade is B"); // Displaying the results to the console
+                 caculated_letter = "B";
+                //return caculated_letter; 
             }
 
             else if (marks >= 77 && marks < 80)
             {
-                Console.WriteLine("The Letter grade is B-"); // Displaying the results to the console
+                 caculated_letter = "B-";
+                //return caculated_letter;
             }
 
             else if (marks >= 74 && marks < 77)
             {
-                Console.WriteLine("The Letter grade is C+"); // Displaying the results to the console
+                 caculated_letter = "C+";
+                //return caculated_letter;
             }
 
             else if (marks >= 70 && marks < 74)
             {
-                Console.WriteLine("The Letter grade is C"); // Displaying the results to the console
+                 caculated_letter = "C";
+                //return caculated_letter;
             }
 
             else if (marks >= 67 && marks < 70)
             {
-                Console.WriteLine("The Letter grade is C-"); // Displaying the results to the console
+                 caculated_letter = "C-";
+                //return caculated_letter;
             }
 
             else if (marks >= 63 && marks < 67)
             {
-                Console.WriteLine("The Letter grade is D+"); // Displaying the results to the console
+                 caculated_letter = "D+";
+                //return caculated_letter;
             }
 
             else if (marks >= 60 && marks < 63)
             {
-                Console.WriteLine("The Letter grade is D"); // Displaying the results to the console
+                 caculated_letter = "D";
+                //return caculated_letter;
             }
 
             else if (marks >= 0 && marks < 60)
             {
-                Console.WriteLine("The Letter grade is F"); // Displaying the results to the console
+                 caculated_letter = " ";
+                //return caculated_letter;
             }
+            return caculated_letter;
 
-            else
-            {
-                Console.WriteLine("Please enter a number between 0 and 100");
-            }
         } // end of Cal_grade
 
     } //end of class
